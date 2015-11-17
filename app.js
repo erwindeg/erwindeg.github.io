41afc6eb35a217bb64947f79d2c98ec7c797dc02
erwindeg <erwin@edegier.nl>
2015-11-17 09:37:00 +0100

    added touch

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+var app = angular.module('app', ['ngRoute', 'ngAnimate','ngTouch']);
 
 app.config(function($routeProvider) {
     $routeProvider
