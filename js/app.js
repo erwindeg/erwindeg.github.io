0dceda3a21018750c945b3a56d1e1ec257585d21
Erwin De Gier <erwin@edegier.nl>
2016-01-23 16:25:27 +0100

    added multi language articles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-var app = angular.module('app', ['ngResource']);
+var app = angular.module('app', ['ngResource', 'pascalprecht.translate']).config(function($translateProvider) {
+	$translateProvider.useStaticFilesLoader({
+		prefix: '/languages/',
+	    suffix: '.json'
+	});
+	$translateProvider.preferredLanguage('nl');
+	$translateProvider.useSanitizeValueStrategy('escape');
+});
 
-app.controller('mainController', function($scope,$resource) {
+app.controller('mainController', function($translate,$scope,$resource,$rootScope) {
     $scope.content = $resource('content/content.json').query();
+});
+
+app.controller('translateController', function($translate, $scope, $rootScope) {
+  $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
+	$rootScope.key = langKey;
+  };
+  $rootScope.key = $translate.proposedLanguage() || $translate.use();
 });
