9a1e9252eb5fd5fd5f90798c4a9f3e64ec6505ed
erwindeg <erwin@edegier.nl>
2016-01-24 19:51:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,4 +33,4 @@ app.controller('translateController', function($translate, $scope, $rootScope) {
 });
 
 
-$(function() { $('a[href*=#]:not([href=#])').click(function() { if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) { var target = $(this.hash); target = target.length ? target : $('[name=' + this.hash.slice(1) +']'); if (target.length) { $('html,body').animate({ scrollTop: target.offset().top - 100 }, 1000); return false; } } });});
+$(function() { $('a[href*=#]:not([href=#])').click(function() { if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) { var target = $(this.hash); target = target.length ? target : $('[name=' + this.hash.slice(1) +']'); if (target.length) { $('html,body').animate({ scrollTop: target.offset().top - 100 }, 700); return false; } } });});
