4d95957b644707038ccb0f85c2035515b170c72c
Erwin De Gier <erwin@edegier.nl>
2016-02-05 22:04:06 +0100

    scroll top animation

--- js/app.js
+++ js/app.js
@@ -32,5 +32,4 @@ app.controller('translateController', function($translate, $scope, $rootScope) {
   $rootScope.key = $translate.proposedLanguage() || $translate.use();
 });
 
-
 $(function() { $('a[href*=#]:not([href=#])').click(function() { if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) { var target = $(this.hash); target = target.length ? target : $('[name=' + this.hash.slice(1) +']'); if (target.length) { $('html,body').animate({ scrollTop: target.offset().top - 100 }, 700); return false; } } });});
